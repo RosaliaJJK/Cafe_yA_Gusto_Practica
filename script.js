5006aac9a3082883c5a04bc495ea57cd1fb48799
RosaliaJJK <pruebaboot117@gmail.com>
2026-05-20 22:58:22 -0600

    actualizacion

--- script.js
+++ script.js
@@ -53,3 +53,59 @@ formulario.addEventListener("submit", async (e) => {
     }
 
 });
+
+// =========================
+// CARRUSEL PROMOCIONES
+// =========================
+
+const track = document.querySelector('.carousel-track');
+const slides = document.querySelectorAll('.slide');
+const nextBtn = document.querySelector('.next');
+const prevBtn = document.querySelector('.prev');
+
+let currentIndex = 0;
+
+function updateCarousel() {
+
+    track.style.transform =
+        `translateX(-${currentIndex * 100}%)`;
+
+}
+
+nextBtn.addEventListener('click', () => {
+
+    currentIndex++;
+
+    if (currentIndex >= slides.length) {
+        currentIndex = 0;
+    }
+
+    updateCarousel();
+
+});
+
+prevBtn.addEventListener('click', () => {
+
+    currentIndex--;
+
+    if (currentIndex < 0) {
+        currentIndex = slides.length - 1;
+    }
+
+    updateCarousel();
+
+});
+
+// AUTO PLAY
+
+setInterval(() => {
+
+    currentIndex++;
+
+    if (currentIndex >= slides.length) {
+        currentIndex = 0;
+    }
+
+    updateCarousel();
+
+}, 4000);
